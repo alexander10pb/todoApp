e60833d7c73326096b1658c083df3ae9e3312ea4
alexander10pb <alexanderpb010@gmail.com>
2024-01-24 20:11:50 -0500

    [add]Creando un Signal

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -17,7 +17,7 @@ export class LabsComponent {
     'Crear componentes',
     'Crear servicio'
   ];
-  name = 'Alexander';
+  name = signal('Alexander');
   age = 22;
   disabled = true;
   img = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Frailsware.com%2Fblog%2Fwp-content%2Fuploads%2F2018%2F05%2FReact-VS-Angular.jpg&f=1&nofb=1&ipt=554a7fbbb066de955e005c73430ab32f1215c6bfb0a0483015ed8c15b82c8a84&ipo=images';
@@ -33,7 +33,9 @@ export class LabsComponent {
   }
 
   changeHandler(event: Event){
-    console.log(event);
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue)
   }
 
   keydownHandler(event: KeyboardEvent){
