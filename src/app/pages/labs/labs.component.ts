7018b872e18342a5aea2e0b00056f9d8899610b5
alexander10pb <alexanderpb010@gmail.com>
2024-01-27 15:27:16 -0500

    [add] Clases en Angular

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
@@ -32,6 +32,13 @@ export class LabsComponent {
   colorCtrol = new FormControl()
   widhthCtrol = new FormControl(50, {
     nonNullable: true,
+  })
+  nameCtrl = new FormControl('Alexander', {
+    nonNullable: true,
+    validators: [
+      Validators.required,
+      Validators.minLength(3)
+    ]
   })
 
   constructor() {
