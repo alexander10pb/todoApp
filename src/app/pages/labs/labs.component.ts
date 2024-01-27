0a9a0929759504e1d85358f7ec699e21d367cb9b
alexander10pb <alexanderpb010@gmail.com>
2024-01-27 13:30:36 -0500

    [add]Controlando un input con form

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
@@ -27,6 +28,14 @@ export class LabsComponent {
     age: 17,
     avatar: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Frailsware.com%2Fblog%2Fwp-content%2Fuploads%2F2018%2F05%2FReact-VS-Angular.jpg&f=1&nofb=1&ipt=554a7fbbb066de955e005c73430ab32f1215c6bfb0a0483015ed8c15b82c8a84&ipo=images'
   });
+
+  colorCtrol = new FormControl()
+
+  constructor() {
+    this.colorCtrol.valueChanges.subscribe(value => {
+      console.log(value)
+    })
+  }
 
   clickHandler(){
     alert('Hola')
